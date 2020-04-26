9d0fe9f5703867c6743629aa940612a6d4d1194e
Phillip Huang <phillip96huang@gmail.com>
2020-04-26 16:02:01 -0400

    Style unchecked rows

--- src/components/debt_row.jsx
+++ src/components/debt_row.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
 
 const DebtRow = ({ debt, checked, handleToggleCheck }) => {
+  const checkedClass = checked ? "" : "unchecked"
+  
   return (
     <>
       <input type="checkbox" 
         checked={checked}
         onChange={handleToggleCheck} />
 
-      <div className="creditor">
+      <div className={checkedClass}>
         {debt.creditorName}
       </div>
-      <div className="first-name">
+      <div className={checkedClass}>
         {debt.firstName}
       </div>
-      <div className="last-name">
+      <div className={checkedClass}>
         {debt.lastName}
       </div>
-      <div className="min-pay-percent right-align">
+      <div className={checkedClass + " min-pay-percent right-align"}>
         {parseFloat(debt.minPaymentPercentage).toFixed(2)}
       </div>
-      <div className="balance right-align">
+      <div className={checkedClass + " balance right-align"}>
         {parseFloat(debt.balance).toFixed(2)}
       </div>
     </>

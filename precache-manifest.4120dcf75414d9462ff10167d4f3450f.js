44a49d9cd6d1c39543bbfa98be77a3369f9a8e39
Phillip Huang <phillip96huang@gmail.com>
2020-04-27 12:14:27 -0400

    Updates

---
+++ precache-manifest.4120dcf75414d9462ff10167d4f3450f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2d99e5cfab5b109c5f381d439a9ba95",
+    "url": "/stratfs-js-test/index.html"
+  },
+  {
+    "revision": "6317f44644003f615f49",
+    "url": "/stratfs-js-test/static/css/main.a1a53093.chunk.css"
+  },
+  {
+    "revision": "6132f8797141e195911d",
+    "url": "/stratfs-js-test/static/js/2.9a392e7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stratfs-js-test/static/js/2.9a392e7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6317f44644003f615f49",
+    "url": "/stratfs-js-test/static/js/main.06d2cb97.chunk.js"
+  },
+  {
+    "revision": "00e2dbca3e3f6b032d45",
+    "url": "/stratfs-js-test/static/js/runtime-main.4fe01d01.js"
+  }
+]);
